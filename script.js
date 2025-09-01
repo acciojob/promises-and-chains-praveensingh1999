f630ad56552579eb9824e6a1b55be312984b0d08
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-09-01 10:57:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+
+function submitform() {
+	const age= document.querySelector("#age").value;
+const name= document.querySelector("#name").value;
+if(!age || !name){
+	alert("Please enter valid details.");
+}
+	if(age && name)
+	{
+	let promise1 = new Promise((res,rej)=>{
+		setTimeout(()=>{
+			if(age>18){
+				res("Welcome, . You can vote.");
+			}
+			else{
+				rej("Oh sorry . You aren't old enough.");
+			}
+		},4000)
+	})
+	promise1
+        .then(result => {
+          alert(result);
+        })
+        .catch(error => {
+          alert(error);
+        });
+
+      return false;
+	}
+}
+
